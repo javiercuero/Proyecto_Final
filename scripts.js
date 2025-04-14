4cbf20f25a500a8603d3c5bf17bc2a08d1d4d280
javiercuero <javierkuero@gmail.com>
2025-04-13 19:33:12 -0500

    Se realiza ajuste del footer, se vincunlan paginas

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// con este codigo se realiza animacion de la Barra de navegacion (nav)
 const nav = document.querySelector("nav");
 const span = nav.querySelector("span");
 const links = nav.querySelectorAll("ul li a");
@@ -9,4 +10,13 @@ links.forEach((link, i) => {
           span.style.width = width + "px";
           span.style.left = left + "px";
           });
+});
+
+// Con este codigo se realiza ajuste responsive de la barra de navegacion (nav)
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+     const menu = document.querySelector('nav ul');
+     menu.classList.toggle('active');
+     
+     // Cambiar el ícono (opcional)
+     this.textContent = menu.classList.contains('active') ? '✕' : '☰';
 });
